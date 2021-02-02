554f2ed402d52a305b537118ff0764fdaf557b70
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-01 16:23:48 -0800

    add more functionality, UI logic and count words in entry
    Co-authored-by: Jer Miller <gkstaff@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -2,3 +2,16 @@ export function Entry(title, body) {
   this.title = title;
   this.body = body;
 }
+Entry.prototype.numOfWords = function() {
+  let titleCount = (this.title).replace(/[ ]{2,}/g, " ");
+  let bodyCount = (this.body).replace(/[ ]{2,}/g, " ");
+  titleCount = titleCount.split(" ").length;
+  bodyCount = bodyCount.split(" ").length;
+  return titleCount + bodyCount;
+};
+export function vowels() {
+
+}
+export function getTeaser() {
+
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { Entry } from './journal.js'
-import './css/styles.css'
-import $ from jquery
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.css'
+import { Entry } from './journal.js';
+import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+$("form").submit(function(event) {
+  event.preventDefault();
+  const title = $("#journalTitle").val();
+  const body = $("#journalBody").val();
+  let entry = new Entry(title, body);
+  let words = entry.numOfWords();
+  console.log(words);
+});
